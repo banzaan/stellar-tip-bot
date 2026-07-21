ae88a6e6383bff85cc28919f18556a756c8bc04c
banzaan <147139866+banzaan@users.noreply.github.com>
2026-07-21 05:43:49 +0330

    Update next.config.mjs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: ['@creit.tech/stellar-wallets-kit', '@stellar/freighter-api'],
 };
 
 export default nextConfig;
